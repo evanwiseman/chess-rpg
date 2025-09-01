79b5fa17b01d6dbdfe98e283aafaeaa97ed399e6
evanwiseman <evanwiseman2000@gmail.com>
2025-09-01 09:32:43 -0700

    added uv and pygame

--- src/main.py
+++ src/main.py
@@ -1,5 +1,28 @@
+import pygame
+
+from src.constants import SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_FRAMERATE
+from src.game import Player
+
+
 def main():
-    print("Hello World")
+    pygame.init()
+    print("Starting Chess-RPG")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player("Player1")
+
+    # Time
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        # Check for exit condition
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.display.flip()
+        dt = clock.tick(SCREEN_FRAMERATE) / 1000 
 
 
 if __name__ == "__main__":
